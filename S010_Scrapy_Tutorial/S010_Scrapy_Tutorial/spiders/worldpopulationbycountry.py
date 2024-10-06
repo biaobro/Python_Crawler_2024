509a4c3e01093dc2187b6728ad4860a317bb01d5
biaobro <biaobro@gmail.com>
2024-10-06 22:53:05 +0800

    get link list of the page

--- S010_Scrapy_Tutorial/S010_Scrapy_Tutorial/spiders/worldpopulationbycountry.py
+++ S010_Scrapy_Tutorial/S010_Scrapy_Tutorial/spiders/worldpopulationbycountry.py
@@ -8,9 +8,19 @@ class WorldpopulationbycountrySpider(scrapy.Spider):
 
     def parse(self, response):
         title = response.xpath('//h1/text()').get()
-        countries = response.xpath('//td/a/text()').getall()
 
-        yield {
-            'title': title,
-            'countries': countries
-        }
+        # 得到国家名称列表，结果是字符串列表
+        # countries = response.xpath('//td/a/text()').getall()
+
+        # 得到国家 xpath 对象
+        countries = response.xpath('//td/a')
+
+        for country in countries:
+            countryName = country.xpath(".//text()").get()
+            countryLink = country.xpath(".//@href").get()
+
+            yield {
+                # 'title': title,
+                'countryName': countryName,
+                'countryLink': countryLink
+            }
