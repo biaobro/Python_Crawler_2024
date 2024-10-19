89f98b8692c1accf8c3fb949de0ffe1143809196
biaobro <biaobro@gmail.com>
2024-10-19 20:44:31 +0800

    add spider to crawl the first page

---
+++ S011_Scrapy_API/S011_Scrapy_API/spiders/quotes.py
@@ -0,0 +1,19 @@
+import json
+
+import scrapy
+
+
+class QuotesSpider(scrapy.Spider):
+    name = "quotes"
+    allowed_domains = ["quotes.toscrape.com"]
+    start_urls = ["https://quotes.toscrape.com/api/quotes?page=1"]
+
+    def parse(self, response):
+        json_resp = json.loads(response.body)
+        quotes = json_resp.get('quotes')
+        for quote in quotes:
+            yield {
+                'author': quote.get('author').get('name'),
+                'tags': quote.get('tags'),
+                'quotes': quote.get('text')
+            }
